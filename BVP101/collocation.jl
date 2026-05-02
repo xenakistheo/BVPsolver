67adb42c62cb2b6f53f359e92f2125ac24fb285b
xenakistheo <theoxen03@gmail.com>
2026-05-02 10:22:29 -0400

    Fix bugs, add OrdinaryDiffEq

--- BVP101/collocation.jl
+++ BVP101/collocation.jl
@@ -1,29 +1,22 @@
 using Optimization
 using OptimizationOptimJL
 using ADTypes
-
-rosenbrock(x, p) = (p[1] - x[1])^2 + p[2] * (x[2] - x[1]^2)^2
-x0 = zeros(2)
-p = [1.0, 100.0]
-
-prob = OptimizationProblem(rosenbrock, x0, p)
-
-using OptimizationOptimJL
-sol = solve(prob, NelderMead())
+using ForwardDiff
+using Plots
+using BenchmarkTools
+using OrdinaryDiffEq
 
 
-
-
-function F!(t, u, p)
+function F!(u, p, t)
     """u0 = 1"""
     return -u 
 end
 
 
 
-function G!(t, u, p)
+function G!(u, p, t)
     """u0 = 1"""
-    return -1000*(u - cos(t)) - sin(t)
+    return -100000*(u - cos(t)) - sin(t)
 end 
 
 
@@ -36,10 +29,10 @@ function collocation_step(f, yn, h, tn, c; p=nothing)
     """
     s = length(c)
     
-    u(x, t) = yn + sum([x[i]*t^i for i in 1:s])
-    uprime(x, t) = sum([i*x[i]*t^(i-1) for i in 1:s])
+    u(x, t) = yn + sum([x[i]*(t-tn)^i for i in 1:s])
+    uprime(x, t) = sum([i*x[i]*(t-tn)^(i-1) for i in 1:s])
     
-    l(x, p, i) = (uprime(x, tn + c[i]*h) - f(tn + c[i]*h, u(x, tn + c[i]*h), p))^2
+    l(x, p, i) = (uprime(x, tn + c[i]*h) - f(u(x, tn + c[i]*h), p, tn + c[i]*h))^2
     loss_fn(x, p) = sum([l(x, p, i) for i in 1:s])
 
     x0 = yn*ones(s)
@@ -47,27 +40,54 @@ function collocation_step(f, yn, h, tn, c; p=nothing)
     prob = OptimizationProblem(optf, x0, p)
     sol = solve(prob, LBFGS())
 
-    return sol
+    return sol.u
 end
 
-collocation_step(F!, 1, 0.1, 0, c)
+collocation_step(F!, 1, 0.1, 0, RadauIIa3_points)
 
 function collocation_solve(f, y0, tspan, dt; c=RadauIIa3_points)
+    s = length(c)
     T = range(tspan[1], tspan[2]; step = dt)
     Y = similar(T)
     Y[1] = y0
     yn = y0 
+    coeffs = zeros(s)
 
-    for (i, t) in enumerate(tspan[1:end-1])
-        coeffs = collocation_step(f, yn, dt, tn, c)
-        Y[i+1] = yn +  sum([coeffs[k]*(t+dt)^k for k in 1:s])
+    for (i, tn) in enumerate(T[1:end-1])
+        coeffs .= collocation_step(f, yn, dt, tn, c)
+        Y[i+1] = yn + sum([coeffs[k]*dt^k for k in 1:s])
         yn = Y[i+1]
     end 
     return T, Y
 end 
     
+tspan = (0, 20)
+dt = 0.00002 # For λ = 100.000
+# dt = 0.000002 # For λ = 1.000.000 # Breaks RAM
+# dt = 0.000000002 # For λ = 10.000.000# No convergence. Breaks RAM. 
 
-tc, yc = collocation_solve(F!, 1, (0, 1), 0.01)
 
-plot(tc, yc)
+prob = ODEProblem(G!, 1, tspan)
+@benchmark solve(prob, RK4(); dt=dt, adaptive=false) 
+#Median: 96.051 ms for for λ = 100.000
+sol = solve(prob, RK4(); dt=dt, adaptive=false)
+trk4, yrk4 = sol.t, sol.u
 
+@benchmark collocation_solve(G!, 1, tspan, 0.2) 
+# Median: 84.712ms for λ = 100.000,  
+# Median: 111.275ms for λ = 1.000.000
+# Median: 122.611ms for λ = 10.000.000
+tc, yc = collocation_solve(G!, 1, tspan, 0.2)
+
+
+plot(tc, yc, label="Collocation")
+plot!(trk4, yrk4, label="RK4", linestyle=:dash)
+
+"""
+As we can see for the examples above, for (super)-stiff problems, 
+    the collocation method is just much better. It seems almost completely 
+    unaffected by the stiffness of the problem. Yes, a bit more compute
+    overhead. However, one should also consider that I did not optimise it 
+    one bit. 
+
+"""
